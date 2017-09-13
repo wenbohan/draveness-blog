ceaac6901f72c88e6f6d88d27c1deecab5f4cba3
draveness <i@draveness.me>
2017-09-14 01:20:06 +0800

    fix: white screen bug when first load on iphone

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -2,10 +2,12 @@ $(window).scroll(function (event) {
     var scroll = $(window).scrollTop();
     var absoluteY = 636;
     var fixedY = 20;
-    if (scroll >= absoluteY - fixedY) {
-      $("#markdown-toc").addClass('markdown-toc-fixed')
-      console.log(scroll);
-    } else {
-      $("#markdown-toc").removeClass('markdown-toc-fixed')
+    if ($(window).width() >= 1400) {
+      if (scroll >= absoluteY - fixedY) {
+        $("#markdown-toc").addClass('markdown-toc-fixed')
+        console.log(scroll);
+      } else {
+        $("#markdown-toc").removeClass('markdown-toc-fixed')
+      }
     }
 });
