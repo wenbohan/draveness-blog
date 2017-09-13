0f7db1daa41aa69f1e85d0b58576e9f3055c92db
draveness <i@draveness.me>
2017-09-14 01:33:03 +0800

    fix: update toc.js

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -1,11 +1,12 @@
+var toc = $("#markdown-toc");
+var absoluteY = toc.offset().top;
+
 $(window).scroll(function (event) {
     var scroll = $(window).scrollTop();
-    var absoluteY = 636;
     var fixedY = 20;
     if (scroll >= absoluteY - fixedY) {
-      $("#markdown-toc").addClass('markdown-toc-fixed')
-      console.log(scroll);
+      toc.addClass('markdown-toc-fixed')
     } else {
-      $("#markdown-toc").removeClass('markdown-toc-fixed')
+      toc.removeClass('markdown-toc-fixed')
     }
 });
