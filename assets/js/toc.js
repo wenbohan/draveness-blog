26085dead0c1fbd64021b37d65b315cbc4421022
draveness <i@draveness.me>
2017-09-14 01:25:53 +0800

    set visibility to hidden

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -2,12 +2,10 @@ $(window).scroll(function (event) {
     var scroll = $(window).scrollTop();
     var absoluteY = 636;
     var fixedY = 20;
-    if ($(window).width() >= 1400) {
-      if (scroll >= absoluteY - fixedY) {
-        $("#markdown-toc").addClass('markdown-toc-fixed')
-        console.log(scroll);
-      } else {
-        $("#markdown-toc").removeClass('markdown-toc-fixed')
-      }
+    if (scroll >= absoluteY - fixedY) {
+      $("#markdown-toc").addClass('markdown-toc-fixed')
+      console.log(scroll);
+    } else {
+      $("#markdown-toc").removeClass('markdown-toc-fixed')
     }
 });
