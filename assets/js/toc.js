5542443458f188850e93f656931c4de95c8b45b1
draveness <i@draveness.me>
2017-09-14 09:57:23 +0800

    fix: toc bug

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -4,7 +4,7 @@ var absoluteY = toc.offset().top;
 $(window).scroll(function (event) {
     var scroll = $(window).scrollTop();
     var fixedY = 20;
-    if (scroll >= absoluteY - fixedY) {
+    if (scroll >= absoluteY - fixedY && $(window).width() >= 1400) {
       toc.addClass('markdown-toc-fixed')
     } else {
       toc.removeClass('markdown-toc-fixed')
