5e4481d5de9f099eb23d6c82338e0f8ada272294
draveness <i@draveness.me>
2017-10-21 20:16:30 +0800

    fix; update toc.scss and js

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -4,9 +4,19 @@ var absoluteY = toc.offset().top;
 $(window).scroll(function (event) {
     var scroll = $(window).scrollTop();
     var fixedY = 20;
-    if (scroll >= absoluteY - fixedY && $(window).width() >= 1400) {
+    if (scroll >= absoluteY - fixedY && $(window).width() >= 1450) {
       toc.addClass('markdown-toc-fixed')
     } else {
       toc.removeClass('markdown-toc-fixed')
     }
+});
+
+$("#markdown-toc a").click(function() {
+  $("html, body").animate({
+    scrollTop: $($(this).attr("href")).offset().top + "px"
+  }, {
+    duration: 600,
+    easing: "swing"
+  });
+  return false;
 });
