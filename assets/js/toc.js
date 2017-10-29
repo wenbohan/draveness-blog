5940b292d340800dd1ab5cd73082b0b602df4b15
draveness <i@draveness.me>
2017-10-29 18:15:05 +0800

    fix: update toc js and css

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -2,6 +2,17 @@ var toc = $(".section-nav");
 var contentTop = $(".post-content").offset().top;
 var absoluteY = toc.offset().top;
 
+//window.addEventListener('scroll', (e) => {
+//  const contentEle = document.querySelector('.post-content')
+//  const tocEle = document.querySelector('.section-nav')
+//
+//  if (window.scrollY - contentEle.offsetTop > 20) {
+// tocEle.classList.add('fixed')
+//  } else {
+// tocEle.classList.remove('fixed')
+//  }
+//})
+
 $(window).scroll(function (event) {
   //var scroll = $(window).scrollTop();
   //var fixedY = 20;
